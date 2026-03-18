e6b7f884c48ac8011f8043721ee9bc58ea81de01
Thuc Nguyen Duy <nguyenduythuc1608@gmail.com>
2026-03-19 00:28:26 +0700

    Update CareTrans.py

--- ultralytics/nn/backbone/CareTrans.py
+++ ultralytics/nn/backbone/CareTrans.py
@@ -328,7 +328,7 @@ def CARETrans_S1(pretrained='', **kwargs):
                 ratios=((2, 4), (2, 4), (4, 4), (4, 4)),
                 **kwargs)
     if pretrained:
-        state_dict = torch.load(pretrained, map_location="cpu")
+        state_dict = torch.load("/kaggle/input/models/mrowlrl/care/pytorch/default/1/CARETrans_S1.pth.tar", map_location="cpu")
         model.load_state_dict(state_dict, strict=False)
         print(f'Load weights successfully')
     return model
