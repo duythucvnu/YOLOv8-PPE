65d4842ecef735ea27a51c4efed2e6552ff0a193
Thuc Nguyen Duy <nguyenduythuc1608@gmail.com>
2026-03-24 22:30:28 +0700

    Commit

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -120,6 +120,7 @@ from ultralytics.nn.neck.BiFPN import BiFPN_Concat2, BiFPN_Concat3
 #from ultralytics.nn.neck.HiFuse import *
 #from ultralytics.nn.neck.Slim import *
 from ultralytics.nn.neck.DySample import *
+from ultralytics.nn.neck.AdaptiveHead import *
 
 
 class BaseModel(torch.nn.Module):
@@ -349,7 +350,7 @@ class BaseModel(torch.nn.Module):
         self = super()._apply(fn)
         m = self.model[-1]  # Detect()
         if isinstance(
-            m, Detect
+            m, Detect, ADDWConvHead
         ):  # includes all Detect subclasses like Segment, Pose, OBB, WorldDetect, YOLOEDetect, YOLOESegment
             m.stride = fn(m.stride)
             m.anchors = fn(m.anchors)
@@ -461,7 +462,7 @@ class DetectionModel(BaseModel):
 
         # Build strides
         m = self.model[-1]  # Detect()
-        if isinstance(m, Detect):  # includes all Detect subclasses like Segment, Pose, OBB, YOLOEDetect, YOLOESegment
+        if isinstance(m, Detect, ADDWConvHead):  # includes all Detect subclasses like Segment, Pose, OBB, YOLOEDetect, YOLOESegment
             s = 256  # 2x min stride
             m.inplace = self.inplace
 
@@ -1736,7 +1737,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
         #elif m in {MLLAttention}:
         #    c2 = ch[f]
         #    args = [c2, *args]
-        elif m in frozenset({Detect, WorldDetect, Segment, Pose, OBB, ImagePoolingAttn, v10Detect}):
+        elif m in frozenset({Detect, WorldDetect, Segment, Pose, OBB, ImagePoolingAttn, v10Detect, ADDWConvHead}):
             args.append([ch[x] for x in f])
         elif m is RTDETRDecoder:  # special case, channels arg must be passed in index 1
             args.insert(1, [ch[x] for x in f])
@@ -1943,6 +1944,8 @@ def guess_model_task(model):
             return "pose"
         if m == "obb":
             return "obb"
+        else:
+            return "detect"            
 
     # Guess from model cfg
     if isinstance(model, dict):
@@ -1965,7 +1968,7 @@ def guess_model_task(model):
                 return "pose"
             elif isinstance(m, OBB):
                 return "obb"
-            elif isinstance(m, (Detect, WorldDetect, YOLOEDetect, v10Detect)):
+            elif isinstance(m, (Detect, WorldDetect, YOLOEDetect, v10Detect, ADDWConvHead)):
                 return "detect"
 
     # Guess from model filename
