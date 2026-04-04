7c598710ba089ddfa7e5e2c6ce439a868b69cf0c
Thuc Nguyen Duy <nguyenduythuc1608@gmail.com>
2026-04-04 20:02:07 +0700

    Commit

--- ultralytics/engine/validator.py
+++ ultralytics/engine/validator.py
@@ -195,8 +195,8 @@ class BaseValidator:
             Profile(device=self.device),
             Profile(device=self.device),
         )
-        #bar = TQDM(self.dataloader, desc=self.get_desc(), total=len(self.dataloader))
-        bar = TQDM(self.dataloader, desc=self.get_desc(), total=len(self.dataloader), disable=True)
+        bar = TQDM(self.dataloader, desc=self.get_desc(), total=len(self.dataloader))
+        #bar = TQDM(self.dataloader, desc=self.get_desc(), total=len(self.dataloader), disable=True)
         self.init_metrics(de_parallel(model))
         self.jdict = []  # empty before each val
         for batch_i, batch in enumerate(bar):
