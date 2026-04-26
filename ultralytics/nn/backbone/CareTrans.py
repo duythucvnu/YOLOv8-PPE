972836aae2919b704686d6fc9cfce3781661a35f
Thuc Nguyen Duy <nguyenduythuc1608@gmail.com>
2026-04-26 22:37:14 +0700

    commit

--- ultralytics/nn/backbone/CareTrans.py
+++ ultralytics/nn/backbone/CareTrans.py
@@ -197,7 +197,8 @@ class CAREStage(nn.Module):
         drop_path_rates = drop_path_rates or [0.] * depth
         stage_blocks = []
         for i in range(depth):
-            if (stage+1)==4 and (i+1)==depth: last = True
+            #if (stage+1)==4 and (i+1)==depth: last = True
+            if (stage+1)==4 and (i+1)==depth: last = False
             else: last = False
             stage_blocks.append(
                 CAREBlock(
@@ -241,7 +242,8 @@ class CARENets(nn.Module):
     ):
         super().__init__()
         num_stage = len(depths)
-        self.channel = [feat_dims[1] + mem_dims[1], feat_dims[2] + mem_dims[2], kwargs.get('cls_dim', (feat_dims[3] + mem_dims[3]) * ratios[-1][1])]
+        self.channel = [feat_dims[1] + mem_dims[1], feat_dims[2] + mem_dims[2], feat_dims[3] + mem_dims[3]]
+        #self.channel = [feat_dims[1] + mem_dims[1], feat_dims[2] + mem_dims[2], kwargs.get('cls_dim', (feat_dims[3] + mem_dims[3]) * ratios[-1][1])]
         dims = tuple(f + m for f, m in zip(feat_dims, mem_dims))
         att_layer = 2
         if not isinstance(token_mixers, (list, tuple)): token_mixers = [token_mixers] * num_stage
